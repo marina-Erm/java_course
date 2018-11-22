d1cd247455573371a176a6fb939247a3e0d1c5cf
marina-Erm <risha46@yandex.ru>
2018-11-22 22:31:52 +0300

    Создан метод для расчета расстояния между точками

--- sandbox/src/main/java/ru/stqa/course/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Point.java
@@ -4,5 +4,20 @@ public class Point {
     public double x;
     public double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
 
+    }
+
+    public double distanse(double x, double y) {
+
+        double a1 = this.x - x;
+        double b1 = this.y - y;
+        return Math.sqrt(a1 * a1 + b1 * b1);
+    }
+
+    public double distanse(Point z) {
+        return distanse(z.x, z.y);
+    }
 }
--- sandbox/src/main/java/ru/stqa/course/sandbox/Rezult.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Rezult.java
@@ -3,23 +3,16 @@ package ru.stqa.course.sandbox;
 public class Rezult {
 
     public static void main(String[] args) {
-    Point a = new Point ();
-      a.x = 0;
-      a.y = 0;
-    Point b = new Point ();
-      b.x = 30;
-      b.y = 40;
+    Point a = new Point (0,0);
+    Point b = new Point (30,40);
 
-      System.out.println ("Расстояние между точками"+"("+a.x+","+a.y+") и ("+b.x+","+b.y+")"+" = "+distanse (a, b));
+
+      System.out.println ("Расстояние между точками"+"("+a.x+","+a.y+") и ("+b.x+","+b.y+")"+" = "+a.distanse(b));
     }
 
-  public static double distanse (Point a, Point b) {
 
-      double a1 = b.x - a.x;
-      double b1 = b.y - a.x;
-      return Math.sqrt(a1 * a1 + b1 * b1);
 
-  }
+
 
 
 }
