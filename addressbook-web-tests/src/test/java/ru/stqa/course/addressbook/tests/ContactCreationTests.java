0f5296eeb4e295a27e656c6c9e9d4619b4389bf6
marina-Erm <risha46@yandex.ru>
2018-12-12 22:16:05 +0300

    Лямбда-выражения (анонимные функции) для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.course.addressbook.model.ContactData;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -16,20 +17,14 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoContactPage();
-    ContactData contact = new ContactData("test1", "test2", "test3", "test4", "+79110001122", "test@test.com", "test1");
+    ContactData contact = new ContactData("test33", "test2", "test3", "test4", "+79110001122", "test@test.com", "test1");
     app.getContactHelper().createContact(contact, true);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
 
-    int max = 0;
-    for (ContactData c: after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
