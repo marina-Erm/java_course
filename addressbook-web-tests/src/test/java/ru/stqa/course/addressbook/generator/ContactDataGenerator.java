ef0404d2ddec924cbda959e458c670c5dcf4d953
marina-Erm <risha46@yandex.ru>
2018-12-26 23:07:49 +0300

    Анализ опций командной строки

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/generator/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/generator/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.course.addressbook.generator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.course.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,18 +13,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main (String args[]) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String args[]) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+
+    }
+
+    private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts,file);
-}
+        save(contacts, new File(file));
+    }
 
     private static void save(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
-        for (ContactData contact: contacts){
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getName(),contact.getMiddlename(),contact.getLastname(),contact.getNickname(),contact.getPhoto(),contact.getAddress(),contact.getHomePhone(),contact.getPhone(),contact.getWorkPhone(),contact.getEmail(),contact.getEmail2(),contact.getEmail3(),contact.getGroup()));
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getName(), contact.getMiddlename(), contact.getLastname(), contact.getNickname(), contact.getPhoto(), contact.getAddress(), contact.getHomePhone(), contact.getPhone(), contact.getWorkPhone(), contact.getEmail(), contact.getEmail2(), contact.getEmail3()));
         }
         writer.close();
     }
@@ -29,14 +49,14 @@ public class ContactDataGenerator {
 
     private static List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
-        for (int i = 0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withName(String.format("test %s", i)).withMiddlename(String.format("middlename %s", i))
-             .withLastname(String.format("lastname %s", i)).withNickname(String.format("nickname %s", i))
-             .withPhoto(new File("src/test/resources/stru.png")).withAddress(String.format("address %s", i))
-             .withhomePhone(String.format("123456 %s", i)).withPhone(String.format("456789 %s", i))
-             .withworkPhone(String.format("789123 %s", i)).withEmail(String.format("test1@test.com %s", i))
-             .withEmail2(String.format("test2@test.com %s", i)).withEmail3(String.format("test3@test.com %s", i)).withGroup(String.format("test1")));
+                    .withLastname(String.format("lastname %s", i)).withNickname(String.format("nickname %s", i))
+                    .withPhoto(new File("src/test/resources/stru.png")).withAddress(String.format("address %s", i))
+                    .withhomePhone(String.format("123456 %s", i)).withPhone(String.format("456789 %s", i))
+                    .withworkPhone(String.format("789123 %s", i)).withEmail(String.format("test1@test.com %s", i))
+                    .withEmail2(String.format("test2@test.com %s", i)).withEmail3(String.format("test3@test.com %s", i)));
         }
         return contacts;
     }
-    }
+}
